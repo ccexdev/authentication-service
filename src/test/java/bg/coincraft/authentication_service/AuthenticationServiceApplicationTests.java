66cbb25fba8863528e13e9da1217cfeb19e95a47
Georgi Bozhinov <ge.bozhinov@gmail.com>
2025-11-10 20:34:24 +0200

    First commit

---
+++ src/test/java/bg/coincraft/authentication_service/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.coincraft.authentication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
